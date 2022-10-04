f767ecaa83eb9f7ab24f349b690cf385a96e0bc5
Luxray555 <100209081+Luxray555@users.noreply.github.com>
2022-10-04 15:53:57 +0200

    Add files via upload

--- public/js/index.js
+++ public/js/index.js
@@ -15,12 +15,14 @@ let caseGame = document.querySelectorAll('.game .case');
 socket.on('allPosUser', (listInfoUser, casePos) => {
     for(key in listInfoUser){
         if(Object.keys(listInfoUser[key]).length != 0){
+            caseGame[(listInfoUser[key]['xPos']+50*listInfoUser[key]['yPos'])].className = "case";
             caseGame[(listInfoUser[key]['xPos']+50*listInfoUser[key]['yPos'])].classList.add(key);
             caseGame[(listInfoUser[key]['xPos']+50*listInfoUser[key]['yPos'])].setAttribute('pseudo-value', listInfoUser[key]['pseudo']);
         }
     }
     for(key in casePos){
         for(i=0;i<casePos[key].length;i++){
+            caseGame[casePos[key][i]['xPos']+50*casePos[key][i]['yPos']].className = "case";
             caseGame[casePos[key][i]['xPos']+50*casePos[key][i]['yPos']].classList.add(key);
         }
     }
@@ -31,6 +33,7 @@ playBtn.addEventListener('click', () => {
         inputPseudo.value = socket.id;
     }
     document.querySelector('.connect').classList.add('des');
+    document.querySelector('.connect').style.display = "none";
     gameDoc.classList.remove('noPlay');
     socket.emit('userInfo',inputPseudo.value);
     document.addEventListener('keydown', (event) => {
@@ -42,6 +45,32 @@ playBtn.addEventListener('click', () => {
             socket.emit('mouvement','left');
         } else if (event.code === "ArrowRight"){
             socket.emit('mouvement','right');
+        }
+      }, false);
+
+      let startTouch = [null,null];
+      let endTouch = [null,null];
+      
+      document.addEventListener('touchstart', (e) => {
+          startTouch[0] = e.targetTouches[0].pageX;
+          startTouch[1] = e.targetTouches[0].pageY;
+      }, false);
+      
+      document.addEventListener('touchend', (e) => {
+        endTouch[0] = e.changedTouches[0].clientX;
+        endTouch[1] = e.changedTouches[0].clientY;
+        if (Math.abs(startTouch[0]-endTouch[0])>=Math.abs(startTouch[1]-endTouch[1])){
+            if(startTouch[0]-endTouch[0]>0){
+                socket.emit('mouvement','left');
+            }else if(startTouch[0]-endTouch[0]<0){
+                socket.emit('mouvement','right');
+            }
+        }else{
+            if(startTouch[1]-endTouch[1]>0){
+                socket.emit('mouvement','up');
+            }else if(startTouch[1]-endTouch[1]<0){
+                socket.emit('mouvement','down');
+            }
         }
       }, false);
 })
@@ -67,6 +96,8 @@ socket.on('userDespawn', (casePosC , spawn, color) => {
         clearTimeout(timeout1);
     },2000);
     caseGame[(spawn['xPos']+50*spawn['yPos'])].classList.remove(color);
+    caseGame[(spawn['xPos']+50*spawn['yPos'])].setAttribute('pseudo-value', "");
+    caseGame[(spawn['xPos']+50*spawn['yPos'])].borderRadius = "0px";
     for(i=0;i<casePosC.length;i++){
         caseGame[casePosC[i]['xPos']+50*casePosC[i]['yPos']].className = 'case';
     }
@@ -80,8 +111,11 @@ socket.on('refreshPos',(oldPos, newPos, spawn, color) => {
     caseGame[newPos].style.borderRadius = "10px";
 });
 
+socket.on('disconnect', () => {
+    location.reload();
+});
+
 socket.on('reboot', () => {
     location.reload();
 })
-
 
